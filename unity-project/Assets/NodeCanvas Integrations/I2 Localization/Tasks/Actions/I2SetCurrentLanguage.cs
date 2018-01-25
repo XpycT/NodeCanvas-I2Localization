c433ee834b4e9be20415b736fefb77df12c976b6
XpycT <xpyct.top@gmail.com>
2018-01-25 20:26:29 +0200

    removed event broadcasting added Set Current Language

---
+++ unity-project/Assets/NodeCanvas Integrations/I2 Localization/Tasks/Actions/I2SetCurrentLanguage.cs
@@ -0,0 +1,48 @@
+﻿using I2.Loc;
+using NodeCanvas.Framework;
+using ParadoxNotion;
+using ParadoxNotion.Design;
+
+namespace NodeCanvas.Tasks.I2Loc
+{
+    [Name("Set Current Language")]
+    [Category("I2 Localization")]
+    [Description("Set the localization CurrentLanguage")]
+    public class I2SetCurrentLanguage : ActionTask
+    {
+        public BBParameter<string> language;
+        
+        private int _choiceIndex = 0;
+
+        protected override string info
+        {
+            get { return string.Format("Set language"); }
+        }
+
+        protected override void OnUpdate()
+        {
+            bool success = false;
+            if (LocalizationManager.HasLanguage(language.value))
+            {
+                success = true;
+                LocalizationManager.CurrentLanguage = language.value;
+            }
+
+            EndAction(success);
+        }
+
+        ////////////////////////////////////////
+        ///////////GUI AND EDITOR STUFF/////////
+        ////////////////////////////////////////
+        #if UNITY_EDITOR
+        protected override void OnTaskInspectorGUI()
+        {
+            string[] languages = LocalizationManager.GetAllLanguages().ToArray();
+            _choiceIndex = UnityEditor.EditorGUILayout.Popup("Language", _choiceIndex, languages);
+            if (_choiceIndex < 0)
+                _choiceIndex = 0;
+            language.value = languages[_choiceIndex];
+        }
+        #endif
+    }
+}
--- unity-project/Assets/NodeCanvas Integrations/I2 Localization/Tasks/Actions/I2SetNextLanguage.cs
+++ unity-project/Assets/NodeCanvas Integrations/I2 Localization/Tasks/Actions/I2SetNextLanguage.cs
@@ -7,43 +7,28 @@ namespace NodeCanvas.Tasks.I2Loc
 {
     [Name("Set Next Language")]
     [Category("I2 Localization")]
-    [Description(
-        "Set localization CurrentLanguage with the next available language and send a graph event.")]
-    public class I2SetNextLanguage : ActionTask<GraphOwner>
+    [Description("Set localization CurrentLanguage with the next available language and send a graph event.")]
+    public class I2SetNextLanguage : ActionTask
     {
-        [RequiredField] public BBParameter<string> successEvent;
-        [RequiredField] public BBParameter<string> failureEvent;
-
-        public BBParameter<float> delay;
-
-        public bool sendGlobal;
-
-
         protected override string info
         {
-            get
-            {
-                return string.Format("Set localization CurrentLanguage with the next available language");
-            }
+            get { return string.Format("Set next language"); }
         }
 
         protected override void OnUpdate()
         {
-            if (elapsedTime >= delay.value)
+            bool success = false;
+            var languages = LocalizationManager.GetAllLanguages();
+            var idx = languages.IndexOf(LocalizationManager.CurrentLanguage);
+
+            if (idx >= 0 && languages.Count > 1)
             {
-                bool success = false;
-                var languages = LocalizationManager.GetAllLanguages();
-                var idx = languages.IndexOf(LocalizationManager.CurrentLanguage);
-
-                if (idx >= 0 && languages.Count > 1)
-                {
-                    // select next language
-                    LocalizationManager.CurrentLanguage = languages[(idx + 1) % languages.Count];
-                    success = true;
-                }
-
-                EndAction(success);
+                // select next language
+                LocalizationManager.CurrentLanguage = languages[(idx + 1) % languages.Count];
+                success = true;
             }
+
+            EndAction(success);
         }
     }
 }
