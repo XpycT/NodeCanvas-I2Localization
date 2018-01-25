b16a9d64a489c727e8d71fb04ad899765d4fe5fd
XpycT <xpyct.top@gmail.com>
2018-01-25 21:52:17 +0200

    added custom inspector for i2gettranslation

--- unity-project/Assets/NodeCanvas Integrations/I2 Localization/Tasks/Actions/I2GetTranslation.cs
+++ unity-project/Assets/NodeCanvas Integrations/I2 Localization/Tasks/Actions/I2GetTranslation.cs
@@ -9,7 +9,8 @@ namespace NodeCanvas.Tasks.I2Loc
     [Description("Get the translations from global source or from LanguageSource prefab")]
     public class I2GetTranslation : ActionTask
     {
-        public BBParameter<LanguageSource> languageObject;
+        public BBParameter<LanguageSource> languageSource;
+        public BBParameter<bool> selectionMode;
         [RequiredField] public BBParameter<string> term;
         public BBParameter<string> translation;
 
@@ -22,11 +23,61 @@ namespace NodeCanvas.Tasks.I2Loc
         protected override void OnUpdate()
         {
             
-            var termTranslation = languageObject.isNull ? LocalizationManager.GetTranslation(term.value) : languageObject.value.GetTranslation(term.value);
+            var termTranslation = languageSource.isNull ? LocalizationManager.GetTranslation(term.value) : languageSource.value.GetTranslation(term.value);
             if (!translation.isNone)
                 translation.value = termTranslation;
             
             EndAction();
         }
+        
+        ////////////////////////////////////////
+        ///////////GUI AND EDITOR STUFF/////////
+        ////////////////////////////////////////
+#if UNITY_EDITOR
+        protected override void OnTaskInspectorGUI()
+        {
+            languageSource = EditorUtils.BBParameterField("Language Source", languageSource) as BBParameter<LanguageSource>;
+            
+            selectionMode = EditorUtils.BBParameterField("Selection Mode", selectionMode) as BBParameter<bool>;
+
+            if (selectionMode.value)
+            {
+                EditTermSelection();
+            }else{
+                term = EditorUtils.BBParameterField("Term", term) as BBParameter<string>;
+            }
+            
+            translation = EditorUtils.BBParameterField("Translation", translation) as BBParameter<string>;
+        }
+        
+        bool EditTermSelection()
+        {
+            var terms = (!languageSource.isNull) ? languageSource.value.GetTermsList() : LocalizationManager.GetTermsList();
+            terms.Sort(System.StringComparer.OrdinalIgnoreCase);
+            terms.Add("");
+            terms.Add("<inferred from text>");
+            terms.Add("<none>");
+            var aTerms = terms.ToArray();
+
+
+            var index = (term.value == "-" || term.value == "" ? aTerms.Length - 1: 
+                (term.value == " " ? aTerms.Length - 2 : 
+                    System.Array.IndexOf( aTerms, term.value)));
+
+            var newIndex = UnityEditor.EditorGUILayout.Popup("Term", index, aTerms);
+
+            if (index == newIndex) return false;
+
+            term.value = (newIndex < 0 || newIndex == aTerms.Length - 1) ? string.Empty : aTerms[newIndex];
+            if (newIndex == aTerms.Length - 1)
+                term.value = "-";
+            else if (newIndex < 0 || newIndex == aTerms.Length - 2)
+                term.value = string.Empty;
+            else
+                term.value = aTerms[newIndex];
+
+            return true;
+        }
+#endif
     }
 }
